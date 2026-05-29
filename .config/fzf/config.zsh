43defe3d28de55b86e04004135d939e2815fe179
Kevin Burk <kevinburk@macbookpro.lan>
2026-05-29 16:16:14 -0500

    add config files for fzf and fzf-tab

---
+++ .config/fzf/config.zsh
@@ -0,0 +1,17 @@
+export FZF_DEFAULT_OPTS=$'
+  --color=fg:#707a8c
+  --color=bg:#0b0e14
+  --color=hl:#ffcc66
+  --color=fg+:#cbccc6
+  --color=bg+:#191e2a
+  --color=hl+:#ffcc66
+  --color=info:#73d0ff
+  --color=prompt:#707a8c
+  --color=pointer:#ff8f40
+  --color=marker:#e0e1dc
+  --color=spinner:#73d0ff
+  --color=header:#d4bfff
+  --color=border:#4a5064
+  --color=gutter:#191e2a
+'
+
