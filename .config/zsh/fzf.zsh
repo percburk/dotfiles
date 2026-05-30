d4125a66c9889c8e05c0121404209c43cbde994c
kevinmburk <kevin.burk@sebrands.com>
2026-05-30 15:35:11 -0500

    adjust eza aliases and fzf settings for bat preview

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,3 +1,4 @@
+# UI
 export FZF_DEFAULT_OPTS=$'
   --color=fg:#707a8c
   --color=bg:#0b0e14
@@ -13,5 +14,16 @@ export FZF_DEFAULT_OPTS=$'
   --color=header:#d4bfff
   --color=border:#4a5064
   --color=gutter:#191e2a
+  --height=60%
+  --layout=reverse
+  --border=rounded
+  --preview-window=right:65%:wrap:border-left
 '
 
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --strip-cwd-prefix'  # strip-cwd-prefix removes the leading ./ from results
+
+# Ctrl-T uses fd
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+export _FZF_PREVIEW_CMD='bat --color=always --style=plain,numbers --line-range=:500 {}'
+export FZF_CTRL_T_OPTS="--preview '$_FZF_PREVIEW_CMD'"
