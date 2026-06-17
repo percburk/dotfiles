4685bb5bfc823eff973915e6ac3553079d37d7d2
kevinmburk <kevin.burk@sebrands.com>
2026-06-17 11:06:19 -0500

    update lazyvim deps and fix harpoon autosave issue

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -1,3 +1,23 @@
 return {
   "Pocco81/auto-save.nvim",
+  opts = {
+    condition = function(buf)
+      local fn = vim.fn
+      local utils = require("auto-save.utils.data")
+
+      local filetype = fn.getbufvar(buf, "&filetype")
+      local buftype = fn.getbufvar(buf, "&buftype")
+
+      if filetype == "harpoon" then
+        return false
+      end
+
+      -- Only autosave normal file buffers.
+      if buftype ~= "" then
+        return false
+      end
+
+      return fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(filetype, {})
+    end,
+  },
 }
