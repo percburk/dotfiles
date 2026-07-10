eefbedb9fe5e96fddb8f368d1115ec5650793380
kevinmburk <kevin.burk@sebrands.com>
2026-07-10 01:18:03 -0500

    add keymaps for filepath yank or insert in telescope this will be sweet

---
+++ .config/nvim/lua/plugins/extend-telescope.lua
@@ -0,0 +1,64 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = function(_, opts)
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
+
+    local function copy_relative_path(prompt_bufnr)
+      local picker = action_state.get_current_picker(prompt_bufnr)
+      local entry = action_state.get_selected_entry()
+
+      if not entry then
+        return
+      end
+
+      local path = entry.path or entry.filename or entry.value or entry[1]
+
+      if not path then
+        return
+      end
+
+      local cwd = picker.cwd or vim.fn.getcwd()
+
+      local relative_path = path
+
+      if vim.fn.fnamemodify(path, ":p") == path then
+        relative_path = vim.fs.relpath(cwd, path) or path
+      end
+
+      vim.fn.setreg("+", relative_path)
+      vim.notify("Copied: " .. relative_path)
+    end
+
+    local function insert_relative_path(prompt_bufnr)
+      local picker = action_state.get_current_picker(prompt_bufnr)
+      local entry = action_state.get_selected_entry()
+
+      if not entry then
+        return
+      end
+
+      local path = entry.path or entry.filename or entry.value or entry[1]
+      local cwd = picker.cwd or vim.fn.getcwd()
+
+      if vim.fn.fnamemodify(path, ":p") == path then
+        path = vim.fs.relpath(cwd, path) or path
+      end
+
+      actions.close(prompt_bufnr)
+      vim.api.nvim_put({ path }, "c", true, true)
+    end
+
+    opts.defaults = opts.defaults or {}
+    opts.defaults.mappings = opts.defaults.mappings or {}
+    opts.defaults.mappings.i = opts.defaults.mappings.i or {}
+    opts.defaults.mappings.n = opts.defaults.mappings.n or {}
+
+    opts.defaults.mappings.i["<C-y>"] = copy_relative_path
+    opts.defaults.mappings.n["<C-y>"] = copy_relative_path
+    opts.defaults.mappings.i["<C-i>"] = insert_relative_path
+    opts.defaults.mappings.n["<C-i>"] = insert_relative_path
+
+    return opts
+  end,
+}
