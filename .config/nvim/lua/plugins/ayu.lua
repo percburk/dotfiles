03fb4e611e63d85a49ac7969606de78f655fe7b7
percburk <percburk@gmail.com>
2026-08-15 15:52:46 -0500

    herdr config, override line nvim line num color

--- .config/nvim/lua/plugins/ayu.lua
+++ .config/nvim/lua/plugins/ayu.lua
@@ -2,9 +2,15 @@ return {
   {
     "Shatur/neovim-ayu",
     config = function()
+      local colors = require("ayu.colors")
+      colors.generate(false)
+
       require("ayu").setup({
         mirage = false,
         terminal = false,
+        overrides = {
+          LineNr = { fg = colors.fg_idle },
+        },
       })
     end,
   },
